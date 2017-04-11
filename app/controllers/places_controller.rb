b7bd8fa4b18c6c52c206765964a7e4ff2f750092
Robert Polacheck <rpolacheck@gmail.com>
2017-04-11 23:40:49 +0000

    Places Form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,8 +2,14 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.all
+  end
   
+  def page_index
+    @users = User.order(:name).page params[:page]
   end
 
+  def new
+    @place = Place.new
+  end
 
 end
