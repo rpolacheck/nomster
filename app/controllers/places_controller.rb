c22bcdf109e23cbaa484dc5a9316fd2ae3d52462
Robert Polacheck <rpolacheck@gmail.com>
2017-04-15 17:45:18 +0000

    GEO

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -24,8 +24,7 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
-
-    if @places.user != current_user
+    if @place.user != current_user
       return render text: 'Not Allowed', status: :forbidden
     end    
   end
