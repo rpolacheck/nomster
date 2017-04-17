8c84a489d06967a43f7d90f7eb6825ed56fb3a49
Robert Polacheck <rpolacheck@gmail.com>
2017-04-17 23:59:15 +0000

    User comments DB

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,11 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
   paginates_per 5
+  
   validates :name, length: {minimum: 3}, presence: true
   validates :address, presence: true
   validates :description, presence: true
