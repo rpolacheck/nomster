63ade6f6b87bf5f608473de9804d13887b07d684
Robert Polacheck <rpolacheck@gmail.com>
2017-04-22 19:44:15 +0000

    Auto-reply Email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,6 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "bbb5483@yahoo.com", subject: "A comment has been added to your place")
+    mail(to: "bpolache@gmail.com", subject: "A comment has been added to your place")
   end
 end
