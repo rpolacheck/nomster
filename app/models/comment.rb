ed94b2eea8425e72705c9b8ba3b8ae1a106ee1ea
Robert Polacheck <rpolacheck@gmail.com>
2017-04-23 23:55:57 +0000

    Send Email on Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
-
+  after_create :send_comment_email
+  
   RATINGS = {
     'one star': '1_star',
     'two stars': '2_star',
@@ -12,6 +13,10 @@ class Comment < ApplicationRecord
 
   def humanized_rating
     RATINGS.invert[self.rating]
+  end
+
+  def send_comment_email
+    NotificationMailer.comment_added(self).deliver
   end
 
 end
