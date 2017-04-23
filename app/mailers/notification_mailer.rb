46266a2c51a40ccd0d8d4ca75df582f789863c00
Robert Polacheck <rpolacheck@gmail.com>
2017-04-23 23:44:48 +0000

    Email adjustment

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,6 +4,6 @@ class NotificationMailer < ApplicationMailer
   def comment_added(comment)
     @place = comment.place
     @place_owner = @place.user
-    mail(to: @place_owner.email, subject: "A comment has been added to your place")
+    mail(to: @place_owner.email, subject: "A comment has been added to #{@place.name}")
   end
 end
